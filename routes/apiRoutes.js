f60c394b5560968d3bd0fa3731dc2e07b0ff0c01
ant4u <anthonyimontalto@gmail.com>
2019-03-13 18:57:52 -0400

    fixed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,11 @@
-const express = require ("express");
-const router = express.Router(); 
-
+module.exports= function(router){
 // REQUIRE AXIOS FOR API CALLS
 // var axios = require("axios");  
 const phrase_controller = require("../controllers/phraseController");
 // const checklist_controller = require("../controllers/checklistController");
 
   router.get("/api/phrases", phrase_controller.phrase_list_get);
+  // http://localhost:3001/api/phrases
+  
   //   router.get("/api/checklist", phrase_controller.checklist_get)
-
-module.exports = router;
+}
