b7a406a51906fccce46d7bd4a1a6e1162bae56d9
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-03-22 00:07:56 -0400

    added files

--- controllers/stateDeptController.js
+++ controllers/stateDeptController.js
@@ -2,39 +2,47 @@ let countriesList = [];
 
 const Parser = require("rss-parser");
 const parser = new Parser();
-
 (async () => {
   let feed = await parser.parseURL(
     "https://travel.state.gov/_res/rss/TAsTWs.xml"
   );
 
-  //   console.log(feed.items[0]);
-  //   console.log(feed.items[0].content);
-
-  //    Each title will have to be parsed and pla`ced into  an array and assigned a country code.
-
-  //   console.log(feed.title);
-
-  //   console.log("feed length is: " + feed.items.length + " items");
-
   feed.items.forEach(item => {
+    let country = item.title.match(/.+?(?= -)/gm);
+    let level = item.title.match(/([0-9])/gm);
     let countryProfile = {
-      country: item.title.match(/.+?(?= -)/gm),
-      content: item.contentSnippet,
+      country: country,
+      content: item.content,
       title: item.title,
-      level: item.title.match(/Level ?(.*)/gm),
+      level: level,
       link: item.link
     };
 
     countriesList.push(countryProfile);
+  });
 
-    // countries.push(countrieslist);
-    // console.log(countries);
-    // console.log("\n" + item.title + "\n" + item.link + "\n");
-    // console.log({countryProfile})
+  countriesList.forEach(item => {
+    item.country = item.country.toString();
+    item.level = parseInt(item.level);
   });
 })();
 
 module.exports = {
   countriesList: countriesList
 };
+//=============================================================================================
+//=============================================================================================
+//=============================================================================================
+//=============================================================================================
+//=============================================================================================
+//=============================================================================================
+//random comments and logging
+//   console.log(feed.items[0]);
+//   console.log(feed.items[0].content);
+//    Each title will have to be parsed and pla`ced into  an array and assigned a country code.
+//   console.log(feed.title);
+//   console.log("feed length is: " + feed.items.length + " items");
+// countries.push(countrieslist);
+// console.log(countries);
+// console.log("\n" + item.title + "\n" + item.link + "\n");
+// console.log({countryProfile})
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,8 @@ module.exports = function(router) {
   // var axios = require("axios");
   const phrase_controller = require("../controllers/phraseController");
   const travel_advisories = require("../controllers/stateDeptController");
+  const bodyParser = require("body-parser");
+
   // const checklist_controller = require("../controllers/checklistController");
 
   router.get("/api/phrases", phrase_controller.phrase_list_get);
@@ -14,10 +16,15 @@ module.exports = function(router) {
   );
   //localhost:3001/api/populate_phrases
 
-  http: router.get("/api/travel_advisories", (req, res) => {
-    console.log(travel_advisories.countriesList);
-    res.send(travel_advisories.countriesList);
-  });
+  http: router.get(
+    "/api/travel_advisories",
+    bodyParser.urlencoded({ extended: false }),
+    (req, res) => {
+      console.log(travel_advisories.countriesList);
+      res.send(travel_advisories.countriesList);
+    }
+  );
+  //http://localhost:3001/api/travel_advisories
 
   //   router.get("/api/checklist", phrase_controller.checklist_get)
 };
