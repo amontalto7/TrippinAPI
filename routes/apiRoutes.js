93481db3474dbaa27f10775d194b94ec43028e28
Clos Devop <31907938+cgarcia101015@users.noreply.github.com>
2019-03-23 11:57:11 -0400

    setup translate route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,19 @@
-module.exports= function(router){
-// REQUIRE AXIOS FOR API CALLS
-// var axios = require("axios");  
-const phrase_controller = require("../controllers/phraseController");
-// const checklist_controller = require("../controllers/checklistController");
+module.exports = function (router) {
+  // REQUIRE AXIOS FOR API CALLS
+  // var axios = require("axios");  
+  const phrase_controller = require("../controllers/phraseController");
+  // const checklist_controller = require("../controllers/checklistController");
 
   router.get("/api/phrases", phrase_controller.phrase_list_get);
   // http://localhost:3001/api/phrases
-  
+
   router.get("/api/populate_phrases", phrase_controller.populate_default_phrases);
   // http://localhost:3001/api/populate_phrases
 
+  router.get("/api/phrases_translated", phrase_controller.translate);
+  // http://localhost:3001/api/phrases_translated
 
-  
+
+
   //   router.get("/api/checklist", phrase_controller.checklist_get)
 }
