4a0b549e5ade29b5911ebd9a90e4b159380cf4a8
ant4u <anthonyimontalto@gmail.com>
2019-03-24 13:29:41 -0400

    checklist model and seed data

---
+++ models/Checklist.js
@@ -0,0 +1,32 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ChecklistSchema = new Schema({
+  // `headline` is required and of type String
+  item: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  type: {
+    type: String
+  },
+  checked: {
+    type: Boolean,
+    default: false
+  },
+  dateAdded: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Checklist = mongoose.model("Checklist", ChecklistSchema);
+
+// Export the Checklist model
+module.exports = Checklist;
