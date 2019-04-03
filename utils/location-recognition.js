666533e0f7a9500a24280ba1ab3de31784989fac
ant4u <anthonyimontalto@gmail.com>
2019-04-03 11:47:02 -0400

    fixed translation api to use only one API call

--- utils/location-recognition.js
+++ utils/location-recognition.js
@@ -1,16 +1,15 @@
 const axios = require("axios");
-const translate = require("./translate-text")
+const translate = require("./translate-text");
 const recogLang = require("./language_recognition");
 var URL = "https://www.ipapi.co/";
 // console.log(URL);
 URL += "json";
 
-
-module.exports = (phrases, req, res) => axios.get(URL).then(async () => {
+module.exports = (phrases, req, res) =>
+  axios.get(URL).then(async () => {
     // const geoLoc = await recogLang(response.data.ip);
     // console.log(phrases);
     const geoLoc = await recogLang("77.136.14.169");
     // console.log("geoLoc");
     translate(geoLoc.countryCode, phrases, req, res);
-
-});
+  });
