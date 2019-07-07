ad2e015b68c8221bd4a747dde9f8e95880bcf48d
ant4u <anthonyimontalto@gmail.com>
2019-07-07 12:15:45 -0400

    added error handling middleware

--- models/Checklist.js
+++ models/Checklist.js
@@ -26,6 +26,15 @@ var ChecklistSchema = new Schema({
   }
 });
 
+// Error Handling Middleware
+ChecklistSchema.post("save", function(error, doc, next) {
+  if (error.name === "MongoError" && error.code === 11000) {
+    next(new Error("There was a duplicate key error"));
+  } else {
+    next();
+  }
+});
+
 // This creates our model from the above schema, using mongoose's model method
 var Checklist = mongoose.model("Checklist", ChecklistSchema);
 
